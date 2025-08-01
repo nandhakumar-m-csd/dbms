c3b56ef99deba7d07aeb13bed216287e5fc50c56
nandhakumar-m-csd <nandhakumar.m.2024.csd@rajalakshmi.edu.in>
2025-08-01 14:57:51 +0530

    Create exp-2.sql

---
+++ exp-2.sql
@@ -0,0 +1,4 @@
+create table MY_EMPLOYEE(IDs number(4),Last_name varchar(25),firstname varchar(25),Userid varchar(25),salary number(9,2));
+INSERT INTO MY_EMPLOYEE  VALUES(1, 'Patel', 'Ralph', 'rpatel', 895);
+INSERT INTO MY_EMPLOYEE VALUES(2, 'Dancs', 'Betty', 'bdancs', 860);
+select* from MY_EMPLOYEE;
