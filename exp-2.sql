6cd6f3b939b488513b4952d95a5bd7298c0945dc
nandhakumar-m-csd <nandhakumar.m.2024.csd@rajalakshmi.edu.in>
2025-08-26 10:49:09 +0530

    exp-2.sql

--- exp-2.sql
+++ exp-2.sql
@@ -1,4 +1,12 @@
-create table MY_EMPLOYEE(IDs number(4),Last_name varchar(25),firstname varchar(25),Userid varchar(25),salary number(9,2));
+CREATE TABLE MY_EMPLOYEE (
+  ID NUMBER(4) NOT NULL,
+  Last_name VARCHAR2(25),
+  First_name VARCHAR2(25),
+  Userid VARCHAR2(25),
+  Salary NUMBER(9,2)
+);
+
 INSERT INTO MY_EMPLOYEE  VALUES(1, 'Patel', 'Ralph', 'rpatel', 895);
 INSERT INTO MY_EMPLOYEE VALUES(2, 'Dancs', 'Betty', 'bdancs', 860);
-select* from MY_EMPLOYEE;
+
+select * from  MY_EMPLOYEE;
